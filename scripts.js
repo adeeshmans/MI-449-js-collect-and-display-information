a290b49581bd0ac006bc23d807065bf0e4acbab2
Adeesh M Shrestha <adeeshman@gmail.com>
2020-02-15 20:11:54 -0500

    Dating profile creator with JS

--- scripts.js
+++ scripts.js
@@ -1,29 +1,125 @@
 var firstInput = document.getElementById("fname");
 var secondInput = document.getElementById("lname");
-// var discrbeInput = document.getElementById("discription");
-// var emailInput = document.getElementById("email");
-// var phoneInput = document.getElementById("phone");
+var discribeInput = document.getElementById("discription");
+var emailInput = document.getElementById("email");
+var phoneInput = document.getElementById("phone");
 var madlibParagraph1 = document.getElementById("madlib1");
-// var madlibParagraph2 = document.getElementById("madlib2");
+var madlibParagraph2 = document.getElementById("madlib2");
 
+// FIRSTNAME
 firstInput.addEventListener("input", function() {
   var fname = firstInput.value;
   var lname = secondInput.value;
 
-  madlibParagraph1.innerHTML =
-    "There once were three <strong>" +
-    fname +
-    "</strong> that were very <strong>" +
-    lname +
-    "</strong> because they couldn't stop <strong>";
+  madlibParagraph1.innerHTML = "<strong>" + fname + " " + lname + "</strong>";
+
+  // shows the code and inputs
+  madlibParagraph2.textContent = "<strong>" + fname + " " + lname + "</strong>";
 });
 
-// firstInput.addEventListener("input", updateMadlib);
-// secondInput.addEventListener("input", updateMadlib);
+// LASTNAME
+secondInput.addEventListener("input", function() {
+  var fname = firstInput.value;
+  var lname = secondInput.value;
 
-// var updateMadlib = function() {
-//   var fname = firstInput.value;
-//   var lname = secondInput.value;
+  madlibParagraph1.innerHTML = "<strong>" + fname + " " + lname + "</strong>";
 
-//   madlibParagraph1.innerHTML = "Hi, my name is" + fname + lname;
-// };
+  // shows the code and inputs
+  madlibParagraph2.textContent = "<strong>" + fname + " " + lname + "</strong>";
+});
+
+// brief
+discribeInput.addEventListener("input", function() {
+  var fname = firstInput.value;
+  var lname = secondInput.value;
+  var discription = discribeInput.value;
+
+  madlibParagraph1.innerHTML =
+    "<strong>" +
+    fname +
+    " " +
+    lname +
+    " " +
+    discription +
+    "</strong> is <em>Hella Charming</em>!!!";
+
+  // shows the code and inputs
+  madlibParagraph2.textContent =
+    "<strong>" +
+    fname +
+    " " +
+    lname +
+    " " +
+    discription +
+    "</strong> is <em>Hella Charming</em>!!!";
+});
+
+// Contact
+emailInput.addEventListener("input", function() {
+  var fname = firstInput.value;
+  var lname = secondInput.value;
+  var discription = discribeInput.value;
+  var email = emailInput.value;
+
+  madlibParagraph1.innerHTML =
+    "<strong>" +
+    fname +
+    " " +
+    lname +
+    " " +
+    discription +
+    "</strong> is <em>Hella Charming</em>!!!" +
+    " <strong>Contact info </strong><strong>" +
+    email +
+    " ";
+
+  // shows the code and inputs
+  madlibParagraph2.textContent =
+    "<strong>" +
+    fname +
+    " " +
+    lname +
+    " " +
+    discription +
+    "</strong> is <em>Hella Charming</em>!!!" +
+    " <strong>Contact info </strong><strong>" +
+    email +
+    " ";
+});
+
+phoneInput.addEventListener("input", function() {
+  var fname = firstInput.value;
+  var lname = secondInput.value;
+  var discription = discribeInput.value;
+  var email = emailInput.value;
+  var phone = phoneInput.value;
+
+  madlibParagraph1.innerHTML =
+    "<strong>" +
+    fname +
+    " " +
+    lname +
+    " " +
+    discription +
+    "</strong> is <em>Hella Charming</em>!!!" +
+    " Contact info <strong>" +
+    email +
+    " " +
+    phone +
+    "</strong>";
+
+  // shows the code and inputs
+  madlibParagraph2.textContent =
+    "<strong>" +
+    fname +
+    " " +
+    lname +
+    " " +
+    discription +
+    "</strong> is <em>Hella Charming</em>!!!" +
+    " <strong>Contact info </strong><strong>" +
+    email +
+    " " +
+    phone +
+    "</strong>";
+});
