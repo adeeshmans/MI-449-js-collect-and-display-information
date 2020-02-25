a04f02c2743c37ea0a7f4521877a915493534e1f
Adeesh M Shrestha <adeeshman@gmail.com>
2020-02-25 16:02:35 -0500

    Email and phone links now call the number and the email that the user inputs

--- scripts.js
+++ scripts.js
@@ -13,27 +13,11 @@ var updateMadlib = function () {
   var discription = discribeInput.value
   var email = emailInput.value
   var phone = phoneInput.value
+  var string = '<h1>Hi, my name is ' + fname + ' ' + lname + '</h1>' + ' ' + '<p>' + discription + '</p>' + ' <p>If you are interested in a date, you can email me at <a href="mailto:' + email + '" target="_blank ">' +
+    email + '</a>' + ' ' + 'or give me a call at <a href="tel:' + phone + ' " target = "_blank" > ' + phone + ' </a>' + '</p > '
 
-  madlibParagraph1.innerHTML =
-    '<h1>Hi, my name is ' + fname + ' ' + lname + '</h1>' +
-    ' ' + '<p>' + discription + '</p>' +
-
-    '<em>Hella Charming</em>!!!' +
-    ' <p>If you are interested in a date, you can email me at <a href="mailto:email" target="_blank">' +
-    email + '</a>' +
-
-    ' ' + 'or give me a call at <a href="tel:phone" target="_blank">' + phone + '</a>' + '</p>'
-
-  madlibParagraph2.textContent =
-    '<h1>Hi, my name is ' + fname + ' ' + lname + '</h1>' +
-    ' ' + '<p>' + discription + '</p>' +
-
-    '<em>Hella Charming</em>!!!' +
-
-    '<p>If you are interested in a date, you can email me at <a href="mailto:email" target="_blank">' +
-    email + '</a>' +
-
-    ' ' + 'or give me a call at <a href="tel:phone" target="_blank">' + phone + '</a>' + '</p>'
+  madlibParagraph1.innerHTML = string
+  madlibParagraph2.textContent = string
 }
 
 firstInput.addEventListener('input', updateMadlib)
@@ -41,122 +25,3 @@ secondInput.addEventListener('input', updateMadlib)
 discribeInput.addEventListener('input', updateMadlib)
 emailInput.addEventListener('input', updateMadlib)
 phoneInput.addEventListener('input', updateMadlib)
-
-// Long version of the code
-// // FIRSTNAME
-// firstInput.addEventListener('input', function () {
-//   var fname = firstInput.value
-//   var lname = secondInput.value
-
-//   madlibParagraph1.innerHTML = '<strong>' + fname + ' ' + lname + '</strong>'
-
-//   // shows the code and inputs
-//   madlibParagraph2.textContent = '<strong>' + fname + ' ' + lname + '</strong>'
-// })
-
-// // LASTNAME
-// secondInput.addEventListener('input', function () {
-//   var fname = firstInput.value
-//   var lname = secondInput.value
-
-//   madlibParagraph1.innerHTML = '<strong>' + fname + ' ' + lname + '</strong>'
-
-//   // shows the code and inputs
-//   madlibParagraph2.textContent = '<strong>' + fname + ' ' + lname + '</strong>'
-// })
-
-// // brief
-// discribeInput.addEventListener('input', function () {
-//   var fname = firstInput.value
-//   var lname = secondInput.value
-//   var discription = discribeInput.value
-
-//   madlibParagraph1.innerHTML =
-//     '<strong>' +
-//     fname +
-//     ' ' +
-//     lname +
-//     ' ' +
-//     discription +
-//     '</strong> is <em>Hella Charming</em>!!!'
-
-//   // shows the code and inputs
-//   madlibParagraph2.textContent =
-//     '<strong>' +
-//     fname +
-//     ' ' +
-//     lname +
-//     ' ' +
-//     discription +
-//     '</strong> is <em>Hella Charming</em>!!!'
-// })
-
-// // Contact
-// emailInput.addEventListener('input', function () {
-//   var fname = firstInput.value
-//   var lname = secondInput.value
-//   var discription = discribeInput.value
-//   var email = emailInput.value
-
-//   madlibParagraph1.innerHTML =
-//     '<strong>' +
-//     fname +
-//     ' ' +
-//     lname +
-//     ' ' +
-//     discription +
-//     '</strong> is <em>Hella Charming</em>!!!' +
-//     ' <strong>Contact info </strong><strong>' +
-//     email +
-//     ' '
-
-//   // shows the code and inputs
-//   madlibParagraph2.textContent =
-//     '<strong>' +
-//     fname +
-//     ' ' +
-//     lname +
-//     ' ' +
-//     discription +
-//     '</strong> is <em>Hella Charming</em>!!!' +
-//     ' <strong>Contact info </strong><strong>' +
-//     email +
-//     ' '
-// })
-
-// phoneInput.addEventListener('input', function () {
-//   var fname = firstInput.value
-//   var lname = secondInput.value
-//   var discription = discribeInput.value
-//   var email = emailInput.value
-//   var phone = phoneInput.value
-
-//   madlibParagraph1.innerHTML =
-//     '<strong>' +
-//     fname +
-//     ' ' +
-//     lname +
-//     ' ' +
-//     discription +
-//     '</strong> is <em>Hella Charming</em>!!!' +
-//     ' Contact info <strong>' +
-//     email +
-//     ' ' +
-//     phone +
-//     '</strong>'
-
-//   // shows the code and inputs
-//   madlibParagraph2.textContent =
-//     '<strong>' +
-//     fname +
-//     ' ' +
-//     lname +
-//     ' ' +
-//     discription +
-//     '</strong> is <em>Hella Charming</em>!!!' +
-//     ' <strong>Contact info </strong><strong>' +
-//     email +
-//     ' ' +
-//     phone +
-//     '</strong>'
-// })
